2198e1be8bc7d1cb9f2025ec11bb325030da1334
javier <jagalu@campusaula.com>
2025-01-21 10:58:35 +0100

    Paint Funcional

--- Tema3/Practica8.js
+++ Tema3/Practica8.js
@@ -3,6 +3,10 @@ var ctx = canvas.getContext('2d');
 var slider = document.getElementById('slider');
 var color = document.getElementById('colorchanger');
 
+var lastX = 0;
+var lastY = 0;
+var estaPint = false;
+
 color.addEventListener('input', function(){
     ctx.strokeStyle = color.value;
 })
@@ -12,16 +16,41 @@ slider.addEventListener('input', function(){
     ctx.lineWidth = slider.value;
 })
 
-canvas.addEventListener('click', function(event){
+canvas.addEventListener('mousedown', function(event){
 
-    ctx.beginPath();
+        estaPint = true;
 
-    canvas.addEventListener('mousedown', function(){
-        ctx.moveTo(event.clientX, event.clientY);
-        ctx.lineTo(event.clientX, event.clientY);
+        ctx.beginPath(); 
+
+        ctx.moveTo(event.offsetX, event.offsetY);
+        ctx.lineTo(event.offsetX, event.offsetY);
+
         ctx.stroke();
-    })
+        
+})
 
+canvas.addEventListener('mousemove', function(event){
+
+    if(estaPint === true)
+    {
+
+        lastX = event.offsetX;
+        lastY = event.offsetY;
+
+        ctx.lineTo(lastX, lastY);
+        ctx.stroke();
+    }
+    
+
+})
+
+canvas.addEventListener('mouseup', function(event){
+
+    estaPint = false;
     ctx.closePath();
 
-}) 
+})
+
+
+
+
